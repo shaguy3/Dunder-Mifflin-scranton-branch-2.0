5cdc41f2fad019b5ef18787670f838516c4179d8
shaguy3 <shaguy3@gmail.com>
2018-11-15 18:22:19 +0200

    Changed a few buttons

--- src/OfficeApp/Controller.java
+++ src/OfficeApp/Controller.java
@@ -2,6 +2,7 @@ package OfficeApp;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Paint;
 
